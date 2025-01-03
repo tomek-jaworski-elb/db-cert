2598e66a73f58095c79618d8294a41c696959cd8
Tomasz Jaworski <tj.toja@gmail.com>
2025-01-03 20:51:15 +0100

    Added photo stream to entity

--- src/main/java/com/jaworski/dbcert/mappers/StudentMapper.java
+++ src/main/java/com/jaworski/dbcert/mappers/StudentMapper.java
@@ -2,10 +2,21 @@ package com.jaworski.dbcert.mappers;
 
 import com.jaworski.dbcert.entity.Student;
 import com.jaworski.dbcert.dto.StudentDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Component
 public class StudentMapper {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StudentMapper.class);
 
     public Student mapToStudent(StudentDTO studentDTO) {
         Student student = new Student();
@@ -17,9 +28,13 @@ public class StudentMapper {
         student.setDateEnd(studentDTO.getDateEnd());
         student.setMrMs(studentDTO.getMrMs());
         student.setCertType(studentDTO.getCertType());
+        String photo = studentDTO.getPhoto();
+        InputStream stream = new ByteArrayInputStream(photo.getBytes(StandardCharsets.UTF_8));
+        student.setPhoto(stream);
         return student;
     }
 
+    @Nullable
     public StudentDTO mapToStudentDTO(Student student) {
         StudentDTO studentDTO = new StudentDTO();
         studentDTO.setId(student.getId());
@@ -30,6 +45,14 @@ public class StudentMapper {
         studentDTO.setDateEnd(student.getDateEnd());
         studentDTO.setMrMs(student.getMrMs());
         studentDTO.setCertType(student.getCertType());
-        return studentDTO;
+        InputStream photo = student.getPhoto();
+        try {
+            String base64Photo = Base64.getEncoder().encodeToString(photo.readAllBytes());
+            studentDTO.setPhoto(base64Photo);
+            return studentDTO;
+        } catch (IOException e) {
+            LOG.error(e.getMessage(), e);
+            return null;
+        }
     }
 }
