129c8848497e9b897396b7e515b87b3b050386d7
Tomek <tj.toja@gmail.com>
2025-02-20 14:28:23 +0100

    Basic authorization for rest client

--- src/main/java/com/jaworski/dbcert/rest/StudentsRestClient.java
+++ src/main/java/com/jaworski/dbcert/rest/StudentsRestClient.java
@@ -17,6 +17,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Base64;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -36,9 +37,19 @@ public class StudentsRestClient {
     @Value("${rest.client.custom.connecttimeout}")
     private int connectTimeout;
 
+    @Value("${http.client.username}")
+    private String user;
+
+    @Value("${http.client.password}")
+    private String password;
+
     private final HttpClient httpClient;
 
     private static final Logger LOG = LoggerFactory.getLogger(StudentsRestClient.class);
+
+    private String getAuthorizationHeaderValue(String username, String password) {
+        return "Basic " + Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
+    }
 
     public void sendStudents(Collection<StudentDTO> students) throws JsonProcessingException, RestClientException {
         ObjectMapper objectMapper = new ObjectMapper();
@@ -47,18 +58,23 @@ public class StudentsRestClient {
             HttpRequest build = HttpRequest.newBuilder()
                     .uri(URI.create(restClientHost + "/api/v1" + "/student"))
                     .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .header(HttpHeaders.AUTHORIZATION, getAuthorizationHeaderValue(user, password))
                     .POST(HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(students)))
                     .build();
 
             HttpResponse<String> send = httpClient.sendAsync(build, HttpResponse.BodyHandlers.ofString())
                     .get(readTimeout, TimeUnit.SECONDS);
 
-            LOG.info(send.body());
-            List<Integer> result = objectMapper.readValue(send.body(), new TypeReference<List<Integer>>() {
-            });
-            if (!result.isEmpty()) {
-                LOG.info("New added students count: {}", result.size());
-                LOG.info("New added students id: {}", result);
+            if (send.statusCode() != 200) {
+                throw new RestClientException("Server respond with status code: " + send.statusCode());
+            } else {
+                LOG.info("{}", send.body());
+                List<Integer> result = objectMapper.readValue(send.body(), new TypeReference<List<Integer>>() {
+                });
+                if (!result.isEmpty()) {
+                    LOG.info("New added students count: {}", result.size());
+                    LOG.info("New added students id: {}", result);
+                }
             }
         } catch (ExecutionException | TimeoutException | InterruptedException e) {
             LOG.error(e.getMessage(), e);
